d671a41841f3aa1db758a7676ac0282d35207c11
Tenecia Offutt <tdcraycroft@msn.com>
2019-03-20 18:29:57 -0400

    added new class

---
+++ Recipe-Finder/Recipe-Finder/Ingredients.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Finder
+{
+    class Ingredients
+    {
+        public string Fixing1 { get; set; }
+        public string Fixing2 { get; set; }
+        public string Fixing3 { get; set; }
+    }
+}
---
+++ Recipe-Finder/Recipe-Finder/Preparation Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Finder
+{
+    class Preparation_Time
+    {
+        public string Fixings1 { get; set; }
+        public string Fixings2 { get; set; }
+        public string Fixings3 { get; set; }
+    }
+}
+
